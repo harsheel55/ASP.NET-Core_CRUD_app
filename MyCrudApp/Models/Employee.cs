13ee741e0dcc71d544c4cdc6281de9b1eaf5e8ac
harsheel55 <22it055@charusat.edu.in>
2024-10-10 19:07:12 +0530

    Initial commit

---
+++ MyCrudApp/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Employee
+{
+	public int Id { get; set; }
+
+	[Required]
+	public string Name { get; set; }
+
+	[Required]
+	public string Position { get; set; }
+
+	[Required]
+	[DataType(DataType.Currency)]
+	public decimal Salary { get; set; }
+
+	public string Email { get; set; }
+}
